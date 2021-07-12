82303be34f0ad963a469ad4aa5925fb3678a7917
Vitali Hauzinski <hauzinski@gmail.com>
2021-07-12 14:47:18 +0300

    fix: fix code

--- src/index.js
+++ src/index.js
@@ -38,22 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-  return expr
-    .split(" ")
-    .map((value) =>
-      value
-        .split("")
-        .map((value2) => Object.keys(MORSE_TABLE).find((key) => MORSE_TABLE[key] === value2))
-        .map((value) =>
-          value
-            .split("")
-            .map((value) => (value === "." ? "10" : "11"))
-            .join("")
-        )
-        .map((value) => (value.length < 10 ? "0".repeat(10 - value.length) + value : value))
-        .join("")
-    )
-    .join("**********");
+  let arr = [];
+  let arr2 = [];
+  let arr3 = [];
+
+  let iter = expr.length / 10;
+  for (let i = 0; i < iter; i++) {
+    arr.push(expr.substr(10 * i, 10));
+  }
+  for (let i = 0; i < arr.length; i++) {
+    for (let e = 0; e < 5; e++) {
+      arr2.push(arr[i].substr(2 * e, 2));
+    }
+    arr2.push(" ");
+  }
+  let e = "";
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] === "10") {
+      e += ".";
+    } else if (arr2[i] === "11") {
+      e += "-";
+    } else if (arr2[i] === "**") {
+      e += " ";
+    } else if (arr2[i] === " ") {
+      arr3.push(e);
+      e = "";
+    }
+  }
+  return arr3
+    .map((value) => MORSE_TABLE[value])
+    .map((value) => (value === undefined ? " " : value))
+    .join("");
 }
 
 module.exports = {
